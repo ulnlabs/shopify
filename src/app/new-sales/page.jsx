e5c5b8c95c8a7e4ecdb88e32cfd8574190bddf69
deepath15 <jahangeer7704@gmail.com>
2024-03-11 15:57:12 +0530

    new

---
+++ src/app/new-sales/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NewSales from '../components/navigation/sales/newsales'
+const page = () => {
+  return (
+    <div>
+      <NewSales />
+    </div>
+  )
+}
+
+export default page
