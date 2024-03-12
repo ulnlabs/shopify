31310affcaf4e244e23ad832070572895eabb0b8
gowdaman-dev <damangowdaman@gmail.com>
2024-03-12 07:34:41 +0530

    pos

--- src/app/(dashboard pages)/sales/pos/page.tsx
+++ src/app/(dashboard pages)/sales/pos/page.tsx
@@ -1,17 +1,33 @@
+"use client"
 import DashboardHeader from '@/app/components/dashboard/DashboardHeader'
-import React from 'react'
-import { AiOutlineUser } from 'react-icons/ai'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { AiOutlineUser, AiOutlineUserAdd } from 'react-icons/ai'
 
 function page() {
+  const [showCustomer, setShowCustomer] = useState<boolean | undefined>(false)
+  const [customer, setCustomer] = useState<string | undefined>("")
   return (
-    <div className='w-full h-full px-4 py-2'>
+    <div className='w-full min-h-screen px-4 py-2'>
       <DashboardHeader title='Sales Invoice' breadcrumb={[{ title: "new-sales", path: '/sales/new-sales' }, { title: 'pos', path: '/sales/pos' }]} />
       <div className="flex justify-between">
-        <div className="w-1/2 h-full">
-          <div className="flex item-center justify-between">
-            <div className="flex items-center">
-              <AiOutlineUser />
-              
+        <div className="w-1/2 h-fit">
+          <div className="flex item-center justify-between py-4">
+            <div className="flex items-center gap-2 ">
+              <AiOutlineUser className='text-xl' />
+              <div className="relative px-1 flex flex-col items-center w-fit rounded-md border">
+                <input type="text" name="customersearch" onChange={(e) => { setShowCustomer(true); setCustomer(e.target.value) }} id="" className='w-full p-1 px-2  outline-none ' />
+                {
+                  (showCustomer && customer) && (
+                    <div className="flex flex-col rounded-md bg-white border absolute top-full mt-2 z-[10] w-[100%] h-fit p-2 text-sm">
+                      jjhfuy
+                    </div>
+                  )
+                }
+              </div>
+              <Link href={""}>
+                <AiOutlineUserAdd className='text-xl' />
+              </Link>
             </div>
           </div>
         </div>
