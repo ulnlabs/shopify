73727f8bbd59a574d2444c66f4c86bc9e8568bf0
hariharan7089 <hariharank007089@gmail.comL>
2024-03-14 21:28:51 +0530

    new

--- src/app/components/settings/sitelist/Prefixes.tsx
+++ src/app/components/settings/sitelist/Prefixes.tsx
@@ -3,74 +3,73 @@ import React from 'react'
 function Prefixes() {
   return (
     <div className="">
-      <form action="" className=' lg:col-span-4  grid md:grid-rows-5  md:grid-flow-col grid-col-12 grid-row-12 gap-4 md:gap-9 p-10'>
-        <div className=" grid gap-3">
-          <label htmlFor=""  className='mr-2'>
-            Category <span className=' text-red-600'> *</span>
-          </label>
-          <input type="text" className=' border rounded-md h-8' />
+    <form action="" className='  grid  grid-col-1 lg:grid-cols-2 gap-y-3  p-10'>
+      <div className=" md:grid md:grid-cols-12 grid  p-2 md:text-end  md:gap-x-10  ">
+        <label htmlFor=""  className='mr-2 md:col-span-5 col-span-12 '>
+   Category <span className=' text-red-600'> *</span>
+        </label>
+        <input type="text" className=' border rounded-md h-8 md:col-span-6 col-span-12 ' />
 
-        </div>
-        <div className=" grid gap-3">
-          <label htmlFor="" className='mr-2 '>
-            Supplier <span className=' text-red-600'> *</span>
-          </label>
-          <input type="text" className=' border rounded-md h-8' />
+      </div>
+      <div className=" md:grid md:grid-cols-12 grid  p-2 md:text-end  md:gap-x-10 ">
+        <label htmlFor=""  className='mr-2 md:col-span-5 col-span-12 '>
+       Supplier  <span className=' text-red-600'> *</span>
+        </label>
+        <input type="text" className=' border rounded-md h-8 md:col-span-6 col-span-12 ' />
 
-        </div>
-        <div className="grid gap-3">
-          <label htmlFor="">
-            Purchase Return <span className=' text-red-600'> *</span>
-          </label>
-          <input type="text" className=' border rounded-md h-8' />
+      </div>
+      <div className=" md:grid md:grid-cols-12  grid  p-2 md:text-end  md:gap-x-10 ">
+        <label htmlFor=""  className='mr-2 md:col-span-5 col-span-12 '>
+      Purchase Return  <span className=' text-red-600'> *</span>
+        </label>
+        <input type="text" className=' border rounded-md h-8 md:col-span-6 col-span-12 ' />
 
-        </div>
-        <div className=" grid gap-3">
-          <label htmlFor="">
-            Sales <span className=' text-red-600'> *</span>
-          </label>
-          <input type="text" className=' border rounded-md h-8' />
+      </div>
+      <div className=" md:grid md:grid-cols-12  grid  p-2 md:text-end  md:gap-x-10 ">
+        <label htmlFor=""  className='mr-2 md:col-span-5 col-span-12 '>
+        Sales  <span className=' text-red-600'> *</span>
+        </label>
+        <input type="text" className=' border rounded-md h-8 md:col-span-6 col-span-12 ' />
 
-        </div>
-        <div className="grid gap-3">
-          <label htmlFor="">
-            Expense <span className=' text-red-600'> *</span>
-          </label>
-          <input type="text" className=' border rounded-md h-8' />
+      </div>
+      <div className=" md:grid md:grid-cols-12  grid  p-2 md:text-end  md:gap-x-10 ">
+        <label htmlFor=""  className='mr-2 md:col-span-5 col-span-12 '>
+        Expense  <span className=' text-red-600'> *</span>
+        </label>
+        <input type="text" className=' border rounded-md h-8 md:col-span-6 col-span-12 ' />
 
-        </div>
-        <div className="grid gap-3">
-          <label htmlFor="">
-            Item <span className=' text-red-600'> *</span>
-          </label>
-          <input type="text" className=' border rounded-md h-8' />
+      </div>
+      <div className=" md:grid md:grid-cols-12  grid  p-2 md:text-end  md:gap-x-10 ">
+        <label htmlFor=""  className='mr-2 md:col-span-5 col-span-12 '>
+        item  <span className=' text-red-600'> *</span>
+        </label>
+        <input type="text" className=' border rounded-md h-8 md:col-span-6 col-span-12 ' />
 
-        </div>
-        <div className="grid gap-3">
-          <label htmlFor="">
-            Purchase <span className=' text-red-600'> *</span>
-          </label>
-          <input type="text" className=' border rounded-md h-8' />
+      </div>
+      <div className=" md:grid md:grid-cols-12  grid  p-2 md:text-end  md:gap-x-10 ">
+        <label htmlFor=""  className='mr-2 md:col-span-5 col-span-12 '>
+        Purchas <span className=' text-red-600'> *</span>
+        </label>
+        <input type="text" className=' border rounded-md h-8 md:col-span-6 col-span-12 ' />
 
-        </div>
-        <div className="grid gap-3">
-          <label htmlFor="">
-            Customer <span className=' text-red-600'> *</span>
-          </label>
-          <input type="text" className=' border rounded-md h-8' />
+      </div>
+      <div className=" md:grid md:grid-cols-12  grid  p-2 md:text-end  md:gap-x-10 ">
+        <label htmlFor=""  className='mr-2 md:col-span-5 col-span-12 '>
+        Customer <span className=' text-red-600'> *</span>
+        </label>
+        <input type="text" className=' border rounded-md h-8 md:col-span-6 col-span-12 ' />
 
-        </div>
-        <div className="grid gap-3">
-          <label htmlFor="">
-            Sales Return <span className=' text-red-600'> *</span>
-          </label>
-          <input type="text" className=' border rounded-md h-8' />
+      </div>
+      <div className=" md:grid md:grid-cols-12  grid  p-2 md:text-end  md:gap-x-10 ">
+        <label htmlFor=""  className='mr-2 md:col-span-5 col-span-12 '>
+        Sales Return <span className=' text-red-600'> *</span>
+        </label>
+        <input type="text" className=' border rounded-md h-8 md:col-span-6 col-span-12 ' />
 
-        </div>
-       
-      </form>
-    </div>
-    
+      </div>
+    </form>
+  </div>
+ 
   )
 }
 
