beaa5a62f8c23d27f39ff673a1e975cebda36cfc
hariharan7089 <hariharank007089@gmail.com>
2024-03-15 21:41:37 +0530

    currency in progress

--- src/app/components/settings/currencylist/Currencylist.tsx
+++ src/app/components/settings/currencylist/Currencylist.tsx
@@ -1,29 +1,47 @@
 "use client"
 import React from 'react'
-import DataTable from "@/app/components/datatable/DataTable"
+import { useState } from 'react';
+import DataTable from "@/app/components/settings/currencylist/DT_curr/Currencydata"
+import { c_columns } from "@/app/components/settings/currencylist/DT_curr/C-column";
 
-function Taxlists() {
+
+  // Function to delete a currency from the list
+
+function Currencylist() {
+    const [currencies, setCurrencies] = useState([
+        {
+          id: 1,
+          currencyname: "Dollar"
+        },
+        {
+          id: 2,
+          currencyname: "INR"
+        }
+      ]);
+    const addCurrency = () => {
+        const newCurrencyName = prompt('Enter new currency name:');
+        if (newCurrencyName) {
+          const newCurrency = {
+            id: currencies.length + 1,
+            currencyname: newCurrencyName
+          };
+          setCurrencies([...currencies, newCurrency]);
+        }
+      };
     return (
         <div className="">
             <div className=" h-screen ">
                 
-                <div className="mx-auto w-[95%] mt-3">
-                    <div className=" border-t-2 bg-slate-300 border-violet-500   rounded-md">
+                <div className="mx-auto w-[95%] p-5 mt-3">
+                    <div className=" border-t-2 border-violet-500   rounded-md">
 
                         <div className="flex justify-between p-3">
                             <h1 className='text-sm tracking-[.2rem]'>Unit List</h1>
-                            <button type='submit' className='p-1 bg-[var(--button)] rounded-md text-white text-sm' > + New Unit</button>
+                            <button onClick={()=>addCurrency()} type='submit' className='p-2 bg-[var(--button)]  font-bold rounded-md text-white text-sm' > <span className='text-lg'>+</span> New Unit</button>
                         </div>
-                        <div className="flex justify-between p-2">
-                            <div className=""></div>
-                            <div className="flex gap-2 ">
-                                <label htmlFor="" className='text-sm'>
-                                    Search :
-                                </label>
-                                <input type="text" className='border w-[10rem] rounded-sm border-gray-3' />
-                            </div>
-                        </div>
+                       
                         <div className=" ">
+                            <DataTable columns={c_columns} data={currencies} />
                            
 
 
@@ -36,4 +54,4 @@ function Taxlists() {
     )
 }
 
-export default Taxlists
+export default Currencylist
---
+++ src/app/components/settings/currencylist/DT_curr/Currencydata.tsx
@@ -0,0 +1,184 @@
+"use client";
+import { useState } from "react";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  ColumnFiltersState,
+  VisibilityState,
+  getFilteredRowModel,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuCheckboxItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export default function DataTable<TData, TValue>({
+  columns,
+  data
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnVisibility,
+      columnFilters,
+      rowSelection,
+    },
+  });
+/* here a small tip i like to filter email in the first div you can add your own filter make  your own logic by replace email by your ancestorkey */
+  return (
+    <>
+      
+      <div className="flex items-center p-4">
+        <Input
+          placeholder="Filter Currency ...."
+          value={(table.getColumn("currencyname")?.getFilterValue() as string) ?? ""}
+          onChange={(event) => 
+            table.getColumn("currencyname")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex-1 text-sm text-muted-foreground ">
+        {table.getFilteredSelectedRowModel().rows.length} of{" "}
+        {table.getFilteredRowModel().rows.length} row(s) selected.
+      </div>
+      <div className="flex items-center justify-end space-x-2 p-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
+    </>
+  );
+}
+/* at all you have your own data table to use your data table call this component like this on your code exam : page.tsx    <DataTable columns={c_columns} data={customerData} /> the c_colums is your column replace it with your and data also  for reference how i share my data see dashboard/customers/list/page.tsx ,i hope u got it  */
