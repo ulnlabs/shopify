454024fa1d0ad9bfcd7075ac14f9a00f2b08f2d8
deepath15 <deepath1506@gmail.com>
2024-03-16 15:32:20 +0530

    component created

--- src/app/components/sales/NewSales.tsx
+++ src/app/components/sales/NewSales.tsx
@@ -9,8 +9,7 @@ import { Check, ChevronsUpDown } from "lucide-react"
 import { format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+
 import { Calendar } from "@/components/ui/calendar"
 import { Input } from '@/components/ui/input'
 
@@ -24,19 +23,11 @@ import {
 } from "@/components/ui/command"
 
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { Selector } from "../Custom-shadcn-components/Selector";
+
 
 import { i_columns } from "../datatable/itemColumn";
 import { DataTable } from "../datatable/DataTable";
+import Selections from "./selections";
 
 const cutomerName = [
   {
@@ -73,22 +64,33 @@ interface FormState {
 const sample = [
   {
 
-    name:"Deepath",
-    quantity:2,
-    price:2000,
-    discount:10,
-    tax:1,
-    subtotal:10,
+    name: "Deepath",
+    quantity: 2,
+    price: 2000,
+    discount: 10,
+    tax: 1,
+    subtotal: 10,
   }
-  ]
-  
+]
+
 
 const NewSales = () => {
+
+
+
   const [data, setData] = useState<FormState>({
     customerName: "",
     billStatus: "",
     billDate: new Date(),
   })
+
+  const [statusValue, setStatusValue] = useState("")
+
+  useEffect(() => {
+    setData({ ...data, billStatus: statusValue });
+  }, [statusValue])
+
+
   const { billDate } = data;
   const [customerOpen, setCustomerOpen] = useState<boolean>(false)
   const handleCustomerClick = (label: string): void => {
@@ -102,10 +104,7 @@ const NewSales = () => {
   }
 
   const [openStatus, setOpenStatus] = useState<boolean>(false);
-  const handleStatusClick = (label: string): void => {
-    setData({ ...data, billStatus: label });
-    setOpenStatus(!openStatus);
-  }
+
   const Items = [
     {
       name: 'Customer',
@@ -133,8 +132,6 @@ const NewSales = () => {
   return (
     <div className='px-10 mt-10'>
       <section>
-
-
         <div className="grid grid-cols-12 gap-5 md:gap-10">
           <div className="  relative  col-start-1 md:col-span-6 col-span-full">
             <div className="flex bg-primary-gray  py-1 px-2 rounded-lg border items-center ">
@@ -188,40 +185,9 @@ const NewSales = () => {
             }
           </div>
         </div>
-
-        <div className="mt-5 mb-10 col-span-full relative ">                //status
-
-          <div className="flex items-center bg-primary-gray border py-1 px-2 rounded-lg" >
-            <IoMdContact className="mr-2 h-4 w-4 shrink-0  opacity-50" />
-            <Input placeholder="Status"
-              value={"" || data.billStatus}
-              onClick={() => { setOpenStatus(!openStatus) }}
-              className=" cursor-pointer " readOnly />
-          </div>
-          {
-            openStatus && (
-              <div className="z-10 absolute w-full mt-2 ">
-                <Command className="rounded-lg border  ">
-                  <CommandList>
-                    <CommandGroup>
-                      {status.map((item) => (
-                        <CommandItem key={item.value}
-                          className="cursor-pointer"
-                          onSelect={handleStatusClick}
-                        >
-                          {item.label}
-                        </CommandItem>
-                      ))}
-
-
-                    </CommandGroup>
-                  </CommandList>
-                </Command>
-              </div>
-            )
-          }
+        <div className="mt-5 mb-10 col-span-full relative  ">                {/* status */}
+          <Selections inputData={["Active", "Final"]} cValue={statusValue} placeholder="Status" setCValue={setStatusValue} />
         </div>
-
         <div className="mt-5 relative">
           <div className="flex items-center border py-1 bg-primary-gray px-2 rounded-lg">
             <BiCart className="mr-2 h-4 w-4 shrink-0  opacity-50" />
@@ -267,10 +233,10 @@ const NewSales = () => {
             </div>
           }
         </div>
-
       </section>
-      
-
+      <section>
+        <DataTable columns={i_columns} data={sample} />
+      </section>
       <div className="grid grid-cols-12 grid-rows-4 mt-16 grid-flow-col gap-4">
         <div className="col-start-1 items-center grid col-span-full md:col-span-6 h-auto rounded-lg bg-primary-gray">
           <div className="grid md:grid-cols-4 gap-20 px-5  ">
@@ -283,35 +249,7 @@ const NewSales = () => {
             <input id="Charges" className=" w-full rounded-md px-2 h-8" placeholder="charges" />
           </div>
           <div className="md:col-start-4 col-start-7 h-auto col-end-13 relative md:col-end-7  bg-primary-gray">
-            <div className="flex items-center  bg-white  border py-1 px-2 rounded-lg" >
-              <input placeholder="Status"
-                value={"" || data.billStatus}
-                onClick={() => { setOpenStatus(!openStatus) }}
-                className=" cursor-pointer w-full outline-none" readOnly />
-              <IoMdContact className="mr-2 h-4 w-4 shrink-0  opacity-50" />
-            </div>
-            {
-              openStatus && (
-                <div className="z-10 grid-cols-subgrid w-full absolute mt-2 ">
-                  <Command className="rounded-lg span-3 border  ">
-                    <CommandList>
-                      <CommandGroup>
-                        {status.map((item) => (
-                          <CommandItem key={item.value}
-                            className="cursor-pointer"
-                            onSelect={handleStatusClick}
-                          >
-                            {item.label}
-                          </CommandItem>
-                        ))}
-
-
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </div>
-              )
-            }
+            <Selections inputData={["Active", "Final"]} cValue={statusValue} placeholder="Status" setCValue={setStatusValue} />
           </div>
         </div>
         <div className="grid items-center grid-cols-subgrid grid-rows-subgrid gap-2 col-start-1 px-1 bg-primary-gray col-span-12 md:col-span-6 rounded-lg row-span-1">
@@ -319,35 +257,7 @@ const NewSales = () => {
             <input id="Charges" className=" w-full rounded-md px-2 h-8" placeholder="charges" />
           </div>
           <div className="md:col-start-4 col-start-7 col-end-13 relative md:col-end-7  bg-primary-gray">
-            <div className="flex items-center  bg-white  border py-1 px-2 rounded-lg" >
-              <input placeholder="Status"
-                value={"" || data.billStatus}
-                onClick={() => { setOpenStatus(!openStatus) }}
-                className=" cursor-pointer w-full outline-none" readOnly />
-              <IoMdContact className="mr-2 h-4 w-4 shrink-0  opacity-50" />
-            </div>
-            {
-              openStatus && (
-                <div className="z-10 grid-cols-subgrid w-full absolute mt-2 ">
-                  <Command className="rounded-lg span-3 border  ">
-                    <CommandList>
-                      <CommandGroup>
-                        {status.map((item) => (
-                          <CommandItem key={item.value}
-                            className="cursor-pointer"
-                            onSelect={handleStatusClick}
-                          >
-                            {item.label}
-                          </CommandItem>
-                        ))}
-
-
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </div>
-              )
-            }
+            <Selections inputData={["Active", "Final"]} cValue={statusValue} placeholder="Status" setCValue={setStatusValue} />
           </div>
         </div>
         <div className="grid items-center grid-cols-subgrid grid-rows-subgrid gap-2 col-start-1 px-1 bg-primary-gray col-span-12 md:col-span-6 rounded-lg row-span-1">
@@ -355,46 +265,14 @@ const NewSales = () => {
             <input id="Charges" className=" w-full rounded-md px-2 h-8" placeholder="charges" />
           </div>
           <div className="md:col-start-4 col-start-7 col-end-13 relative md:col-end-7  bg-primary-gray">
-            <div className="flex items-center  bg-white  border py-1 px-2 rounded-lg" >
-              <input placeholder="Status"
-                value={"" || data.billStatus}
-                onClick={() => { setOpenStatus(!openStatus) }}
-                className=" cursor-pointer w-full outline-none" readOnly />
-              <IoMdContact className="mr-2 h-4 w-4 shrink-0  opacity-50" />
-            </div>
-            {
-              openStatus && (
-                <div className="z-10 grid-cols-subgrid w-full absolute mt-2 ">
-                  <Command className="rounded-lg span-3 border  ">
-                    <CommandList>
-                      <CommandGroup>
-                        {status.map((item) => (
-                          <CommandItem key={item.value}
-                            className="cursor-pointer"
-                            onSelect={handleStatusClick}
-                          >
-                            {item.label}
-                          </CommandItem>
-                        ))}
-
-
-                      </CommandGroup>
-                    </CommandList>
-                  </Command>
-                </div>
-              )
-            }
+            <Selections inputData={["Active", "Final"]} cValue={statusValue} placeholder="Status" setCValue={setStatusValue} />
           </div>
         </div>
-
-
-
         <div className="md:col-end-13 md:mt-0 mt-10 h-auto md:col-span-4 rounded-lg col-span-full grid items-center bg-primary-gray">
           <div className="grid  grid-cols-4 lg:grid-cols-3 py-2 justify-start gap-4  px-5 ">
             <p className="col-start-1 col-end-3">Subtotal</p>
             <p className="col-span-2 col-start-3 md-pr-2  ">100000000</p>
           </div>
-
         </div>
         <div className="md:col-end-13 md:col-span-4 py-2 h-auto rounded-lg col-span-full grid items-center bg-primary-gray">
           <div className="grid grid-cols-4 h-auto lg:grid-cols-3 justify-start gap-4  px-5  ">
@@ -415,60 +293,23 @@ const NewSales = () => {
           </div>
         </div>
       </div>
-
+      <section>
+        <DataTable columns={i_columns} data={sample} />
+      </section>
       <section className="grid grid-cols-12 md:gap-10 gap-5">
         <div className="mt-5 col-start-1 col-span-6 relative ">
-
-          <div className="flex items-center bg-primary-gray border py-1 px-2 gap-1 rounded-lg" >
-            <Input placeholder="Status"
-              value={"" || data.billStatus}
-              onClick={() => { setOpenStatus(!openStatus) }}
-              className=" cursor-pointer " readOnly />
-           
-
-            <IoMdContact className="mr-2 h-4 w-4 shrink-0  opacity-50" />
-             
-          </div>
-          {
-            openStatus && (
-              <div className="z-10 absolute w-full mt-2 ">
-                <Command className="rounded-lg border  ">
-                  <CommandList>
-                    <CommandGroup>
-                      {status.map((item) => (
-                        <CommandItem key={item.value}
-                          className="cursor-pointer"
-                          onSelect={handleStatusClick}
-                        >
-                          {item.label}
-                        </CommandItem>
-                      ))}
-
-
-                    </CommandGroup>
-                  </CommandList>
-                </Command>
-              </div>
-            )
-          }
+          <Selections inputData={["Active", "Final"]} cValue={statusValue} placeholder="Status" setCValue={setStatusValue} />
         </div>
         <div className="col-span-6 gird items-center border bg-primary-gray py-1 px-2 rounded-lg col-start-7 mt-5 ">
-          <Input type="text" placeholder="Amount" className="w-full px-2  b "/>
-
+          <Input type="text" placeholder="Amount" className="w-full px-2  b " />
         </div>
-        <div className="col-span-full py-1 px-2 rounded-lg bg-primary-gray">
-          <textarea placeholder="Note" className="h-auto px-1 py-1 w-full" >
-
+        <div className="col-span-full py-2 px-2 rounded-lg bg-primary-gray">
+          <textarea placeholder="Note" className="h-auto px-1 w-full" >
           </textarea>
         </div>
       </section>
-     <section>
-          <DataTable  columns={i_columns} data={sample} />
-
-     </section>
-
     </div>
-    
+
   )
 }
 
