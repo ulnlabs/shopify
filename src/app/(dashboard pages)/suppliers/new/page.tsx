5675d4ed25ea8dd3cf61c92549d54ae9b216d700
jahangeer7704 <jahangeer7704@gmail.com>
2024-03-17 13:46:12 +0530

    suppliers

---
+++ src/app/(dashboard pages)/suppliers/new/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddSupplier from '@/app/components/supplier/AddSupplier'
+function page() {
+  return (
+    <div className='w-full'>
+        <AddSupplier/>
+
+    </div>
+  )
+}
+
+export default page
