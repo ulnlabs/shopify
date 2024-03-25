3715831766a9274eb9be55891e06a8b66140d958
deepath15 <deepath1506@gmail.com>
2024-03-25 16:07:33 +0530

    animated

---
+++ src/app/components/sales-pur/global.d.ts
@@ -0,0 +1,19 @@
+export interface FormState {
+    customerName: string,
+    billDate: Date,
+    billStatus: string,
+    billQuantity: number,
+    billCharges: any,
+    billTaxType: string,
+    billDiscount: any,
+    billDiscountType: string,
+    billNote: string,
+    billSubtotal: number,
+    billOtherCharge: number,
+    billOverallDis: number
+    billTotal: number,
+    billPaymentType: string,
+    billAmount: any,
+  
+  
+  }
