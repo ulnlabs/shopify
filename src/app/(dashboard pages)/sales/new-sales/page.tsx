990f065754fcfa86146d87d3b0282a2f4abe0a13
deepath15 <deepath1506@gmail.com>
2024-04-06 10:39:52 +0530

    switching to coder

--- src/app/(dashboard pages)/sales/new-sales/page.tsx
+++ src/app/(dashboard pages)/sales/new-sales/page.tsx
@@ -144,7 +144,7 @@ const page = () => {
       />
       <div className="flex justify-center pt-5 pb-10 gap-10">
         <button onClick={handleClick} type="button" className="w-20 py-2 bg-primary-save rounded-md text-white">Save</button>
-        <Link href={"../../dashboard"} className="w-20 py-2 text-center bg-primary-close rounded-md text-white">Close</Link>
+        <Link href={"/dashboard"} className="w-20 py-2 text-center bg-primary-close rounded-md text-white">Close</Link>
       </div>
 
       {/* <button onClick={() => {
