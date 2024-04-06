b841e362890313870461b3726bd8057ed71ba1d9
deepath15 <deepath1506@gmail.com>
2024-04-06 19:45:39 +0530

    new

--- src/app/popup/page.tsx
+++ src/app/popup/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import AddTax from "@/app/components/settings/popup/AddTax"
+
+const [taxState, setTaxState] = useState<boolean | null>(false)
 
 function page() {
   return (
     <div>
-        <AddTax />
+        <AddTax close={setTaxState}  />
     </div>
   )
 }
