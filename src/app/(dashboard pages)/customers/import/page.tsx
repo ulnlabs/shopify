7f5f22f49679d64976f108507aa863726e5f4c1f
ulnlabs <ulnlabs@gmail.com>
2024-06-25 10:37:21 +0530

    new

--- src/app/(dashboard pages)/customers/import/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ImportCustomer from '@/app/components/customer/ImportCustomer'
-function page() {
-  return (
-   <ImportCustomer/>
-  )
-}
-
-export default page
--- src/app/(dashboard pages)/suppliers/import/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ImportSupplier from '@/app/components/supplier/ImportSuppliers'
-function page() {
-  return (
-        <ImportSupplier/>
-  )
-}
-
-export default page
